c0a4e3318f3ab8d7ad84cb2b84837576adba88d8
kamalkumar7 <202051098@iiitvadodara.ac.in>
2023-07-21 13:52:51 +0530

    new commit

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ const server = http.createServer(app);
 const io = new Server(server);
 
 
-// app.use(express.static('build'));
+app.use(express.static('build'));
 // app.use((req, res, next) => {
 //     res.sendFile(path.join(__dirname, 'build', 'index.html'));
 // });
